cd2d4ea023f98642cec3108a0bafe258212144d2
mbgshk <mbgshk@gmail.com>
2022-07-11 13:29:35 +0700

    Final Code

--- navbar.cy.js
+++ navbar.cy.js
@@ -1,20 +1,24 @@
 /// <reference types="cypress" />
 
-// Define Steps
+// Final Code
 describe('Navbar Test', () => {
     before(() => {
-        // TODO
+        cy.visit('http://zero.webappsecurity.com/index.html')
     });
 
     it('Should display online banking content', () => {
-        // TODO
+        cy.contains('Online Banking').click()
+        cy.url().should('include', 'online-banking.html')
+        cy.get('h1').should('be.visible')
     });
 
     it('Should display feedback content', () => {
-        // TODO
+        cy.contains('Feedback').click()
+        cy.url().should('include', 'feedback.html')
     });
 
     it('Should display homepage content', () => {
-        // TODO
+        cy.contains('Zero Bank').click()
+        cy.url().should('include', 'index.html')
     });
 });

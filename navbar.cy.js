35aac5dcfc852f56eac851ac9e9371680fdfe100
mbgshk <mbgshk@gmail.com>
2022-07-11 13:17:52 +0700

    Define Stepd

--- navbar.cy.js
+++ navbar.cy.js
@@ -1,2 +1,20 @@
 /// <reference types="cypress" />
 
+// Define Steps
+describe('Navbar Test', () => {
+    before(() => {
+        // TODO
+    });
+
+    it('Should display online banking content', () => {
+        // TODO
+    });
+
+    it('Should display feedback content', () => {
+        // TODO
+    });
+
+    it('Should display homepage content', () => {
+        // TODO
+    });
+});
